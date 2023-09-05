831ecbd6f84af902dc2e5cc396f23f732166fa1d
brajeshbhagbole <brajeshbhagbole@gmail.com>
2023-09-05 10:54:03 +0530

    my firt contribution

--- src/Coatingshery/oncoating/Devanshudoc/Reverse_by_brajesh.java
+++ src/Coatingshery/oncoating/Devanshudoc/Reverse_by_brajesh.java
@@ -2,7 +2,7 @@ package Coatingshery.oncoating.Devanshudoc;
 
 import java.util.Scanner;
 
-public class Reverse_by_brajesh {
+public class Reverse_by_esh {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
         String str = s.nextLine();
