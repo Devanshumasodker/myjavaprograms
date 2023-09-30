369bbf366f017f0ab678d0670e55de4a5854364f
Ankit Rajput <ankitrajputji00@gmail.com>
2023-09-30 22:54:11 +0530

    LoopUpdate

--- src/Array.java
+++ src/Array.java
@@ -5,9 +5,9 @@ public class Array {
         int k = 3,sum = 0;
 
         int[] arr = {1,2,3};//new int[k];
-        for (int i = 0; i < arr.length ; i++) {
-            System.out.println( " the sum of all lement " + sum  + "+" + arr[i]);
-            sum = sum + arr[i];
+        for (int val : arr) {
+            System.out.println( " the sum of all lement " + sum  + "+" + val);
+            sum +=val
           //  System.out.println(sum + "=" + sum  + "+" + arr[i]);
         }
         System.out.println(sum);
