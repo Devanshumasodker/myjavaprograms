8aedbe28d019556be118c03d1a7983114f729ea0
Devanshu Masodker <devanshumasodker357@gmail.com>
2023-10-21 18:30:39 +0530

    Exceptional handling

---
+++ src/Coatingshery/oncoating/mygit/Strong_num.java
@@ -0,0 +1,11 @@
+package Coatingshery.oncoating.mygit;
+
+public class Strong_num {
+
+    public static void main(String[] args) {
+
+        int n = 1;
+        System.out.println((n&(n-1))==0&&n!=0);
+
+    }
+}
---
+++ src/Oops_clg/Inst_blk.java
@@ -0,0 +1,24 @@
+package Oops_clg;
+
+//static int z = 10;
+//static int x = 22;
+//
+//        {
+//        System.out.println(x);
+//        System.out.println("hello from instance-block");
+//        }
+//public static void m1(){
+//        int  x = 40;
+//        System.out.println("hello from m1");
+//        }
+//public static void main(String[] args) {
+//        Inst_blk obj = new Inst_blk();
+//        z = 12;
+//        System.out.println(z);
+
+public class Inst_blk {
+
+    public static void main(String[] args) {
+//TODO: this code is for instance Block
+    }
+}
